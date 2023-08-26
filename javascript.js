9e816654f9bd9937d1ed4deea615403b1fc803a4
solai-y <solaiy17112001@gmail.com>
2023-08-26 17:47:08 +0800

    Constant movement, track reset issue fixed
    The html side now has the data of amount dragged stored so that the webpage is able to continue moving the track from where the user last left off

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ window.onmousedown = e => {
 // The below resets the mouse position when the mouse is released
 window.onmouseup = e => {
     track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 // Tracks mouse movement in comparison with the mouse down position
@@ -20,8 +21,14 @@ window.onmousemove = e => {
         maxDelta = window.innerWidth / 2;
 
     // calculate percentage of movement of the mouse
-    const percentage = (mouseDelta / maxDelta) * -100;
+    const percentage = (mouseDelta / maxDelta) * -100, 
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+        // the code find the new percentage to add and adds it to the prev-percentage dataset that is put into the html this way the data of amount moved is stored and can be used to calculate how much further to move the track when the mouse is down again
+
+        // we add the percentage to this dataset to help transfer the data from this function to the onmouseup event handler which will sotre the value on the html side for referral
+        track.dataset.percentage = nextPercentage;
+        
 
     // adjust style of the track based on the percentage moved
-    track.style.transform = `translate(${percentage}%, -50%)`;
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
 }
