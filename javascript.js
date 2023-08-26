f97991bfb76680973098a8d638a99384cb987782
solai-y <solaiy17112001@gmail.com>
2023-08-26 17:31:02 +0800

    fixed typescript error of null
    Moved script to the bottom of the HTML file

--- javascript.js
+++ javascript.js
@@ -1,19 +1,14 @@
 const track = document.getElementById('imageTrack');
 
+// The below checks the position of the mouse being put down
 window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
+    track.dataset.mouseDownAt=e.clientX;
 }
 
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-}
+// Tracks mouse movement in comparison with the mouse down position
+window.onmousemove = e => {
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
 
-window.onmousedown = e => {
-    if(track.dataset.mouseDownAt === "0") return;
-
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
-
-    const percentage = (mouseDelta / maxDelta) * -100;
-
-    track.style.transform = 'translate(${percentage}%, -50%)';
+    const percentage = (mouseDelta / maxDelta) * 100;
 }
